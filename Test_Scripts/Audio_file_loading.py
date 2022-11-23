fc2c9e24fe5f165abdf29d8f3c484f2446eed0ac
BastianHartmann <bastian.hartmann@hm.edu>
2022-11-23 18:49:37 +0100

    Futher testing

--- Test_Scripts/Audio_file_loading.py
+++ Test_Scripts/Audio_file_loading.py
@@ -7,6 +7,8 @@ Created on Tue Nov 22 14:03:55 2022
 
 #! python3
 
+#%matplotlib notebook
+
 import librosa as lr
 import tkinter as tk
 from tkinter import filedialog
@@ -14,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa.display
 import sounddevice as sd
-import datetime
+import time
 
 root = tk.Tk()
 root.withdraw()
@@ -24,14 +26,43 @@ filePath = filedialog.askopenfilename()
 AudioLoad, SampleRate = lr.load(filePath,sr=None,mono=True)
 
 # Displaying of audio track
-WavePlot = librosa.display.waveshow(AudioLoad[0:SampleRate*60],sr=SampleRate)
+#WavePlot = librosa.display.waveshow(AudioLoad[0:SampleRate*60],sr=SampleRate)
 
 
-# Add time marker at 30 seconds
-markerTime = 30
-ax = plt.gca()
+# Other way of displaying the track
+xPlot = np.linspace(0,len(AudioLoad),len(AudioLoad)+1)
+xPlotTime = xPlot / SampleRate
+Seconds = 60
+SoundFig, SoundAx = plt.subplots()
+SoundAx.plot(xPlotTime[0:int(SampleRate*Seconds)],AudioLoad[0:int(SampleRate*Seconds)])
+SoundAx.set_xlabel("Time [s]")
+SoundAx.set_ylabel("Normalized Audio Signal")
+SoundAx.set_ylim((-1.2,1.2))
+
+
+# Add time markers at 10,30 and 50 seconds with annotation
+MarkerPos = np.array([10,30,50])
+for i in MarkerPos:
+    SoundAx.axvline(i,c='orange',label=i)
+    annText = "Time : %d s "%i
+    SoundAx.text(i,1.17,annText,color='orange',ha='right',va='top', rotation=90, fontweight='bold', fontsize=8)
 
 
 
 # Playing of audio track (first 10 seconds)
-sd.play(AudioLoad[0:SampleRate*10],samplerate=SampleRate)
+
+TimeStart = 0
+TimeEnd = 10
+sd.play(AudioLoad[int(SampleRate*TimeStart):SampleRate*TimeEnd],samplerate=SampleRate)
+StartTime = time.time()
+interval = 1
+PlayBar = SoundAx.axvline(TimeStart,color='red')
+
+for i in range(TimeStart+1,TimeEnd+1):
+    #time.sleep(StartTime + i*interval - time.time())
+    SoundAx.lines[-1].remove()
+    SoundAx.axvline(i,color='red')
+    #display(SoundFig)
+    #clear_output(wait = True)
+    print(i,' s')
+    #plt.pause(1)
