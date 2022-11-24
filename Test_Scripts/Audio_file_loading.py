82f93566eb75c049b24d162c0dfe4d0c6bc97b21
BastianHartmann <bastian.hartmann@hm.edu>
2022-11-24 10:27:50 +0100

    Further testing

--- Test_Scripts/Audio_file_loading.py
+++ Test_Scripts/Audio_file_loading.py
@@ -14,6 +14,7 @@ import tkinter as tk
 from tkinter import filedialog
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import librosa.display
 import sounddevice as sd
 import time
@@ -48,21 +49,38 @@ for i in MarkerPos:
     SoundAx.text(i,1.17,annText,color='orange',ha='right',va='top', rotation=90, fontweight='bold', fontsize=8)
 
 
-
 # Playing of audio track (first 10 seconds)
 
 TimeStart = 0
 TimeEnd = 10
 sd.play(AudioLoad[int(SampleRate*TimeStart):SampleRate*TimeEnd],samplerate=SampleRate)
-StartTime = time.time()
-interval = 1
-PlayBar = SoundAx.axvline(TimeStart,color='red')
 
-for i in range(TimeStart+1,TimeEnd+1):
-    #time.sleep(StartTime + i*interval - time.time())
-    SoundAx.lines[-1].remove()
-    SoundAx.axvline(i,color='red')
-    #display(SoundFig)
-    #clear_output(wait = True)
-    print(i,' s')
-    #plt.pause(1)
+# And draw a time indicator updated every second
+# StartTime = time.time()
+# interval = 1
+# PlayBar = SoundAx.axvline(TimeStart,color='red')
+
+# for i in range(TimeStart+1,TimeEnd+1):
+#     time.sleep(StartTime + i*interval - time.time())
+#     #SoundAx.lines[-1].remove()
+#     #SoundAx.axvline(i,color='red')
+#     PlayBar.set_xdata(i)
+#     plt.gcf().canvas.draw()
+#     plt.gcf().canvas.flush_events()
+#     #display(SoundFig)
+#     #clear_output(wait = True)
+#     print(i,' s')
+#     #plt.pause(1)
+
+# plt.show()
+
+# interval = 1
+PlayBar = SoundAx.axvline(TimeStart,color='red')
+def updatePlayBar(pos):
+    PlayBar.set_xdata(pos)
+    return PlayBar
+
+
+PlayAnimation = animation.FuncAnimation(SoundFig,updatePlayBar,frames = range(TimeStart+1,TimeEnd+1), interval=1)
+PlayBar = SoundAx.axvline(TimeStart,color='red')
+plt.show()
